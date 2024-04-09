efb6a67113ced297734df0196ea5715021383bf4
thinknathan <github@thinknathan.com>
2024-04-08 19:21:22 -0700

    minor formatting and remove unused line

--- prng/src/prng.cpp
+++ prng/src/prng.cpp
@@ -4,91 +4,90 @@ static uint32_t mSeed;
 
 static uint32_t mulberry32()
 {
-    uint32_t z = (mSeed += 0x6D2B79F5);
-    z = (z ^ (z >> 15)) * (z | 1);
-    z ^= z + (z ^ (z >> 7)) * (z | 61);
-    return z ^ (z >> 14);
+	uint32_t z = (mSeed += 0x6D2B79F5);
+	z = (z ^ (z >> 15)) * (z | 1);
+	z ^= z + (z ^ (z >> 7)) * (z | 61);
+	return z ^ (z >> 14);
 }
 
 static int SetSeed(lua_State* L)
 {
-    mSeed = luaL_checkinteger(L, 1);
-    return 0;
+	mSeed = luaL_checkinteger(L, 1);
+	return 0;
 }
 
 static int RandFloat(lua_State* L)
 {
-    float randValue = mulberry32() / 0xFFFFFFFF;
-    lua_pushnumber(L, randValue);
-    return 1;
+	float randValue = mulberry32() / 0xFFFFFFFF;
+	lua_pushnumber(L, randValue);
+	return 1;
 }
 
 static int RandInt(lua_State* L)
 {
-    int maxValue = luaL_checkinteger(L, 1);
-    int randValue = mulberry32() % (maxValue + 1);
-    lua_pushinteger(L, randValue);
-    return 1;
+	int maxValue = luaL_checkinteger(L, 1);
+	int randValue = mulberry32() % (maxValue + 1);
+	lua_pushinteger(L, randValue);
+	return 1;
 }
 
 static int Coin(lua_State* L)
 {
-    int result = mulberry32() % 2;
-    lua_pushinteger(L, result);
-    return 1;
+	int result = mulberry32() % 2;
+	lua_pushinteger(L, result);
+	return 1;
 }
 
 static int Suit(lua_State* L)
 {
-    int result = mulberry32() % 4;
-    lua_pushinteger(L, result);
-    return 1;
+	int result = mulberry32() % 4;
+	lua_pushinteger(L, result);
+	return 1;
 }
 
 static int Dice(lua_State* L)
 {
-    int numDice = luaL_checkinteger(L, 1);
-    int numSides = luaL_checkinteger(L, 2);
-    int modifier = 0;
+	int numDice = luaL_checkinteger(L, 1);
+	int numSides = luaL_checkinteger(L, 2);
+	int modifier = 0;
 
-    if (lua_gettop(L) >= 3) {
-        modifier = luaL_checkinteger(L, 3);
-    }
+	if (lua_gettop(L) >= 3) {
+		modifier = luaL_checkinteger(L, 3);
+	}
 
-    int result = 0;
-    for (int i = 0; i < numDice; ++i) {
-        result += (mulberry32() % numSides) + 1;
-    }
+	int result = 0;
+	for (int i = 0; i < numDice; ++i) {
+		result += (mulberry32() % numSides) + 1;
+	}
 
-    result += modifier;
+	result += modifier;
 
-    lua_pushinteger(L, result);
-    return 1;
+	lua_pushinteger(L, result);
+	return 1;
 }
 
 static const luaL_Reg prng_functions[] =
 {
-    { "set_seed", SetSeed },
-    { "rand_float", RandFloat },
-    { "rand_int", RandInt },
-		{ "coin", Coin },
-    { "suit", Suit },
-    { "dice", Dice },
-    { 0, 0 }
+	{ "set_seed", SetSeed },
+	{ "rand_float", RandFloat },
+	{ "rand_int", RandInt },
+	{ "coin", Coin },
+	{ "suit", Suit },
+	{ "dice", Dice },
+	{ 0, 0 }
 };
 
 static void LuaInit(lua_State* L)
 {
-    int top = lua_gettop(L);
-    luaL_register(L, "prng", prng_functions);
-    lua_pop(L, 1);
+	luaL_register(L, "prng", prng_functions);
+	lua_pop(L, 1);
 }
 
 static dmExtension::Result Initialize(dmExtension::Params* params)
 {
-    LuaInit(params->m_L);
+	LuaInit(params->m_L);
 	dmLogInfo("Registered %s Extension\n", "prng");
-    return dmExtension::RESULT_OK;
+	return dmExtension::RESULT_OK;
 }
 
 DM_DECLARE_EXTENSION(prng, "prng", 0, 0, Initialize, 0, 0, 0)
